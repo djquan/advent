882d380be427c99da5032768b2df27aaa33d15a8
Dan Quan <daniel@quan.io>
2018-12-09 19:58:46 -0800

    Add template

---
+++ main.rs
@@ -0,0 +1,39 @@
+use std::env;
+use std::fs;
+
+fn main() {
+    let filename = env::args().nth(1).expect("Usage: ./run FILENAME");
+    let claims: Vec<String> = fs::read_to_string(filename)
+        .expect("Unable to read file")
+        .lines()
+        .map(|l| l.parse().expect("Invalid data"))
+        .collect();
+    println!("Part one: {}", part_one());
+    println!("Part two: {}", part_two());
+}
+
+fn part_one() -> u32 {
+
+    0
+}
+
+fn part_two() -> u32 {
+
+    0
+}
+
+
+#[cfg(test)]
+mod tests {
+    use crate::{part_one, part_two};
+
+    #[test]
+    fn part_one_works(){
+        assert_eq!(0, part_one())
+    };
+
+    #[test]
+    fn part_two_works(){
+        assert_eq!(0, part_two())
+    };
+}
