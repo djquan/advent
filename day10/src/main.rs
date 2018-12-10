91080a20187ba46c66878cd0bb74df86e8014486
Dan Quan <daniel@quan.io>
2018-12-09 20:24:05 -0800

    fmt

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -13,27 +13,24 @@ fn main() {
 }
 
 fn part_one() -> u32 {
-
     0
 }
 
 fn part_two() -> u32 {
-
     0
 }
-
 
 #[cfg(test)]
 mod tests {
     use crate::{part_one, part_two};
 
     #[test]
-    fn part_one_works(){
+    fn part_one_works() {
         assert_eq!(0, part_one());
     }
 
     #[test]
-    fn part_two_works(){
+    fn part_two_works() {
         assert_eq!(0, part_two())
     }
 }
