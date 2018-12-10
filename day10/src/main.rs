a302638c5a53a7b5379fe786189b1b9a9bde7a4a
Dan Quan <daniel@quan.io>
2018-12-09 22:45:28 -0800

    Add day 10

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -1,36 +1,74 @@
-use std::env;
+extern crate regex;
+
+use regex::Regex;
+use std::collections::HashSet;
 use std::fs;
 
 fn main() {
-    let filename = env::args().nth(1).expect("Usage: ./run FILENAME");
-    let input: Vec<String> = fs::read_to_string(filename)
+    let r = Regex::new(r"position=<(.*), (.*)> velocity=<(.*), (.*)>").unwrap();
+    let input: Vec<(i32, i32, i32, i32)> = fs::read_to_string("input.txt")
+        //    let input: Vec<(i32, i32, i32, i32)> = fs::read_to_string("test_input.txt")
         .expect("Unable to read file")
         .lines()
-        .map(|l| l.parse().expect("Invalid data"))
+        .map(|l| {
+            let captures = r.captures(l.trim()).unwrap();
+            let x: i32 = captures.get(1).unwrap().as_str().trim().parse().unwrap();
+            let y: i32 = captures.get(2).unwrap().as_str().trim().parse().unwrap();
+            let w: i32 = captures.get(3).unwrap().as_str().trim().parse().unwrap();
+            let z: i32 = captures.get(4).unwrap().as_str().trim().parse().unwrap();
+            (x, y, w, z)
+        })
         .collect();
-    println!("Part one: {}", part_one());
-    println!("Part two: {}", part_two());
+    part_one(input, 20000);
 }
 
-fn part_one() -> u32 {
-    0
-}
+fn part_one(input: Vec<(i32, i32, i32, i32)>, times: u32) {
+    let mut input = input.clone();
+    for i in 1..times {
+        input = input
+            .iter()
+            .map(|(x, y, w, z)| (*x + *w, *y + *z, *w, *z))
+            .collect();
 
-fn part_two() -> u32 {
-    0
-}
+        let (mut max_x, mut max_y, _, _) = input[0];
+        let mut min_y = max_y;
+        let mut min_x = max_x;
 
-#[cfg(test)]
-mod tests {
-    use crate::{part_one, part_two};
+        let points: HashSet<(i32, i32)> = input
+            .iter()
+            .map(|(x, y, _, _)| {
+                if *x > max_x {
+                    max_x = *x;
+                }
 
-    #[test]
-    fn part_one_works() {
-        assert_eq!(0, part_one());
-    }
+                if *x < min_x {
+                    min_x = *x;
+                }
 
-    #[test]
-    fn part_two_works() {
-        assert_eq!(0, part_two())
+                if *y > max_y {
+                    max_y = *y;
+                }
+
+                if *y < min_y {
+                    min_y = *y;
+                }
+
+                (*x, *y)
+            })
+            .collect();
+
+        if max_x - min_x < 100 && max_y - min_y < 150 {
+            println!("\nSecond : {}  ---------------------------------------------------------------------\n", i);
+            for i in min_y..=max_y {
+                for j in min_x..=max_x {
+                    if points.contains(&(j, i)) {
+                        print!("#");
+                    } else {
+                        print!(".");
+                    }
+                }
+                println!("\n");
+            }
+        }
     }
 }
